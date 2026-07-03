154fe0b9b3c94a029624f0db8de06510e94044f9
zeljanastamatovic <zeljana.stamatovic77@gmail.com>
2026-07-03 17:43:22 +0200

    attempt

--- getData.py
+++ getData.py
@@ -6,30 +6,45 @@ import csv
 
 def get_departures():
     for i in range(4):
-        odgovor = requests.get(f'https://www.flightstats.com/v2/flight-tracker/departures/HND/?year=2026&month=7&date=1&hour={6*i}')
+        odgovor = requests.get(f'https://www.flightstats.com/v2/flight-tracker/departures/HND/?year=2026&month=7&date=2&hour={6*i}')
         print(odgovor.status_code)
         vsebina = odgovor.text
         with open(f"hndDepartures{i}.html","w",encoding="utf-8") as dat:
             print(vsebina, file=dat)
-            
-            
-with open('urls.txt',"w",encoding='utf-8') as d:
+
+
+def txt_file():
+    with open('urls.txt',"w",encoding='utf-8') as d:
+        for i in range(4):
+
+            with open(f"hndDepartures{i}.html",encoding="utf-8") as dat:
+                vsebina = dat.read()
+                indexes = [x.start() for x in re.finditer('"url"', vsebina)]
+                for index in indexes:
+                    url = "https://www.flightstats.com/v2" + vsebina[index + 7: index + 7 +(vsebina[index + 7:]).find('"')]
+                    ## gleda da li je code share
+                    if vsebina[index + 7: index + 7 + (vsebina[index + 7:]).find('}}')] < vsebina[index + 7: index + 7 +(vsebina[index + 7:]).find('Codeshare')]:
+                        print(url, file=d)
+
+
+#get_departures()
+#txt_file()      
+    
+with open("urls.txt",encoding='utf8')as dat:
     for i in range(4):
+        v = dat.readline()
+    vrstica = v[:len(v)-1]
+    odgovor = requests.get(vrstica)
+    print(odgovor.status_code)
+    vsebina = odgovor.text
+    vrstica = vrstica.replace('\\u0026','&')
+    print(vrstica)
 
-        with open(f"hndDepartures{i}.html",encoding="utf-8") as dat:
-            vsebina = dat.read()
-
-            indexes = [x.start() for x in re.finditer('"url"', vsebina)]
-            print(len(indexes))  # <-- [6, 13, 19]
-            n = len('"url"="')
-            for index in indexes:
-                url = "https://www.flightstats.com/v2" + vsebina[index + 7: index + 7 +(vsebina[index + 7:]).find('"')]
-                print(url, file = d)
-                
-        #indeks = vsebina.find('"url"')
-        #print(vsebina[indeks+10:].find('"'))
-        #print(vsebina[indeks + 7: indeks + 7 +(vsebina[indeks + 7:]).find('"')])
-        ##with open("htmls/hndDepartures.html",encoding="utf-8") as dat:
+    #vsebina = vsebina[vsebina.find(''):]
+    vsebina = vsebina.replace(',',',\n')
+    with open('flight1.txt',"w",encoding='utf-8') as d:
+        print(vsebina, file=d)
+    
     
     
     
